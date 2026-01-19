810383729a475acbbb4c572ee393506d9776fd8d
KOUSHIK R <thunisu69@gmail.com>
2026-01-19 09:39:47 +0530

    Initialize Discord bot with basic commands
    Same as above

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+import os
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('adingu! 🏓')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Hello! naan oru bot hosted on the cloud.')
+
+
